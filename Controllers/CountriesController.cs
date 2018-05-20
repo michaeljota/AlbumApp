64bae55c6398233700dfc70da817a502b30fe259
Michael De Abreu <michaeljota@outlook.com>
2018-05-20 17:31:52 -0400

    feat(country): create countries api endpoint

---
+++ Controllers/CountriesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AlbumApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlbumApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CountriesController : ControllerBase
+    {
+        private AlbumContext Context { get; }
+
+        public CountriesController(AlbumContext context) {
+            Context = context;
+        }
+
+        // GET api/countries
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Context.Countries.ToList());
+        }
+    }
+}
