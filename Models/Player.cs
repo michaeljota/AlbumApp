2bce757ddc5b8506f58c834b6afbbfdc49370f0e
Michael De Abreu <michaeljota@outlook.com>
2018-05-20 19:16:53 -0400

    feat(player): create player model in dotnet app

---
+++ Models/Player.cs
@@ -0,0 +1,10 @@
+namespace AlbumApp.Models
+{
+    public class Player
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+    }
+}
