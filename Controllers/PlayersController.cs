46c3fabd17ed18f951e156c2161364e94fde178b
Michael De Abreu <michaeljota@outlook.com>
2018-05-20 19:18:33 -0400

    feat(player): create players api endpoint

---
+++ Controllers/PlayersController.cs
@@ -0,0 +1,90 @@
+using System.Collections.Generic;
+using System.Linq;
+using AlbumApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlbumApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class PlayersController : Controller
+    {
+
+        private AlbumContext Context { get; }
+
+        public PlayersController(AlbumContext context)
+        {
+            Context = context;
+        }
+
+        // GET api/players
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(Context.Players.ToList());
+        }
+
+        // GET api/players/5
+        [HttpGet("{id}", Name = "GetPlayer")]
+        public IActionResult Get(int id)
+        {
+            Player result = Context.Players.Find(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        // POST api/players
+        [HttpPost]
+        public IActionResult Post([FromBody] Player model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            Context.Players.Add(model);
+            Context.SaveChanges();
+            return CreatedAtRoute("GetPlayer", new { id = model.ID }, model);
+        }
+
+        // PUT api/players/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Player newModel)
+        {
+            var current = Context.Players.Find(id);
+            if (current == null)
+            {
+                return NotFound();
+            }
+
+            current.FirstName = newModel.FirstName ?? current.FirstName;
+            current.LastName = newModel.LastName ?? current.LastName;
+            current.Position = newModel.Position ?? current.Position;
+
+            Context.Players.Update(current);
+
+            Context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // DELETE api/players/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var model = Context.Players.Find(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            Context.Entry(model).State = EntityState.Deleted;
+
+            Context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
